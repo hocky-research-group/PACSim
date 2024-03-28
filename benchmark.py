743172e4facfdcf54b227e2bdf074bacb96b18c3
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2024-03-28 16:15:44 -0400

    Refactored benchmark script

--- benchmark.py
+++ benchmark.py
@@ -1,101 +1,101 @@
+import argparse
 import time
+import subprocess
+import sys
 import numpy as np
 import openmm
 from openmm import app
 from openmm import unit
 from colloids import (ColloidPotentialsParameters, ColloidPotentialsAbstract, ColloidPotentialsAlgebraic,
                       ColloidPotentialsTabulated)
+from colloids.helper_functions import read_xyz_file
 
 
-def load_initxyz(filename):
-    with open(filename, "r") as f:
-        line = next(f)
-        ls = line.split()
-        number_atoms = int(ls[0])
-        _ = next(f)
-        types = np.zeros(number_atoms, dtype=str)
-        pos = np.zeros((number_atoms, 3), dtype=unit.Quantity)
-        for index_atom in range(number_atoms):
-            line = next(f)
-            ls = line.split()
-            types[index_atom] = ls[0]
-            pos[index_atom, 0] = float(ls[1]) * unit.nanometer
-            pos[index_atom, 1] = float(ls[2]) * unit.nanometer
-            pos[index_atom, 2] = float(ls[3]) * unit.nanometer
-    return types, pos
-
-
-def main(platform_name: str = "Reference", potentials: str = "algebraic", use_log: bool = False,
-         number_steps: int = 100) -> None:
-    radius_positive = 105.0 * unit.nanometer
-    radius_negative = 95.0 * unit.nanometer
-    surface_potential_positive = 44.0 * (unit.milli * unit.volt)
-    surface_potential_negative = -54.0 * (unit.milli * unit.volt)
-    parameters = ColloidPotentialsParameters(brush_density=0.09 / (unit.nanometer ** 2),
-                                             brush_length=10.6 * unit.nanometer,
-                                             debye_length=5.726968 * unit.nanometer,
-                                             temperature=298.0 * unit.kelvin,
-                                             dielectric_constant=80.0)
+def benchmark_parameters():
     # noinspection PyUnresolvedReferences
-    collision_rate = 0.01 / unit.picosecond
-    # noinspection PyUnresolvedReferences
-    timestep = 0.05 * unit.picosecond
-    mass_positive = 1.0 * unit.amu
-    mass_negative = (radius_negative / radius_positive) ** 3 * mass_positive
-    side_length = 12328.05 * unit.nanometer
+    parameters = {
+        "radius_positive": 105.0 * unit.nanometer,
+        "radius_negative": 95.0 * unit.nanometer,
+        "surface_potential_positive": 44.0 * (unit.milli * unit.volt),
+        "surface_potential_negative": -54.0 * (unit.milli * unit.volt),
+        "colloid_potentials_parameters": ColloidPotentialsParameters(brush_density=0.09 / (unit.nanometer ** 2),
+                                                                     brush_length=10.6 * unit.nanometer,
+                                                                     debye_length=5.726968 * unit.nanometer,
+                                                                     temperature=298.0 * unit.kelvin,
+                                                                     dielectric_constant=80.0),
+        "collision_rate": 0.01 / unit.picosecond,
+        "timestep": 0.05 * unit.picosecond,
+        "mass_positive": 1.0 * unit.amu,
+        "side_length": 12328.05 * unit.nanometer,
+    }
+    parameters["mass_negative"] = (
+            (parameters["radius_negative"] / parameters["radius_positive"]) ** 3 * parameters["mass_positive"])
+    return parameters
 
-    types, positions = load_initxyz("first_frame.xyz")
+
+def benchmark_openmm(platform_name: str = "Reference", potentials: str = "algebraic", use_log: bool = False,
+                     number_steps: int = 100) -> None:
+    parameters = benchmark_parameters()
+
+    types, positions = read_xyz_file("first_frame.xyz")
 
     topology = app.topology.Topology()
     chain = topology.addChain()
     residue = topology.addResidue("res1", chain)
-    try:
-        app.element.Element(0, "P1", "P1", mass_positive)
-        app.element.Element(1, "N1", "N1", mass_negative)
-    except ValueError:
-        # ValueError is raised if the element already exists.
-        pass
-    for type, position in zip(types, positions):
-        if type == "P":
-            topology.addAtom("Cs", app.element.Element.getBySymbol("P1"), residue)
+    app.element.Element(0, "positive", "positive", parameters["mass_positive"])
+    app.element.Element(1, "negative", "negative", parameters["mass_negative"])
+    for t, position in zip(types, positions):
+        if t == "P":
+            topology.addAtom("positive", app.element.Element.getBySymbol("positive"), residue)
         else:
-            assert type == "N"
-            topology.addAtom("Cl", app.element.Element.getBySymbol("N1"), residue)
-    topology.setPeriodicBoxVectors(np.array([[side_length.value_in_unit(unit.nanometer), 0.0, 0.0],
-                                             [0.0, side_length.value_in_unit(unit.nanometer), 0.0],
-                                             [0.0, 0.0, side_length.value_in_unit(unit.nanometer)]]))
+            assert t == "N"
+            topology.addAtom("negative", app.element.Element.getBySymbol("negative"), residue)
+    topology.setPeriodicBoxVectors(np.array([[parameters["side_length"].value_in_unit(unit.nanometer), 0.0, 0.0],
+                                             [0.0, parameters["side_length"].value_in_unit(unit.nanometer), 0.0],
+                                             [0.0, 0.0, parameters["side_length"].value_in_unit(unit.nanometer)]]))
 
     system = openmm.System()
-    system.setDefaultPeriodicBoxVectors(openmm.Vec3(side_length.value_in_unit(unit.nanometer), 0.0, 0.0),
-                                        openmm.Vec3(0.0, side_length.value_in_unit(unit.nanometer), 0.0),
-                                        openmm.Vec3(0.0, 0.0, side_length.value_in_unit(unit.nanometer)))
-    platform = openmm.Platform.getPlatformByName(platform_name)
-    integrator = openmm.LangevinIntegrator(parameters.temperature, collision_rate, timestep)
+    system.setDefaultPeriodicBoxVectors(
+        openmm.Vec3(parameters["side_length"].value_in_unit(unit.nanometer), 0.0, 0.0),
+        openmm.Vec3(0.0, parameters["side_length"].value_in_unit(unit.nanometer), 0.0),
+        openmm.Vec3(0.0, 0.0, parameters["side_length"].value_in_unit(unit.nanometer)))
+    # Prevent printing the traceback when the platform is not existing.
+    try:
+        platform = openmm.Platform.getPlatformByName(platform_name)
+    except openmm.OpenMMException as err:
+        print(err, file=sys.stderr)
+        exit(1)
+    integrator = openmm.LangevinIntegrator(parameters["colloid_potentials_parameters"].temperature,
+                                           parameters["collision_rate"],
+                                           parameters["timestep"])
 
     if potentials == "algebraic":
         colloid_potentials: ColloidPotentialsAbstract = ColloidPotentialsAlgebraic(
-            colloid_potentials_parameters=parameters, use_log=use_log)
+            colloid_potentials_parameters=parameters["colloid_potentials_parameters"], use_log=use_log)
     else:
         assert potentials == "tabulated"
         colloid_potentials: ColloidPotentialsAbstract = ColloidPotentialsTabulated(
-            radius_one=radius_positive, radius_two=radius_negative,
-            surface_potential_one=surface_potential_positive, surface_potential_two=surface_potential_negative,
-            colloid_potentials_parameters=parameters, use_log=use_log)
+            radius_one=parameters["radius_positive"], radius_two=parameters["radius_negative"],
+            surface_potential_one=parameters["surface_potential_positive"],
+            surface_potential_two=parameters["surface_potential_negative"],
+            colloid_potentials_parameters=parameters["colloid_potentials_parameters"], use_log=use_log)
 
-    for type, position in zip(types, positions):
-        if type == "P":
-            system.addParticle(mass_positive)
-            colloid_potentials.add_particle(radius=radius_positive, surface_potential=surface_potential_positive)
+    for t, position in zip(types, positions):
+        if t == "P":
+            system.addParticle(parameters["mass_positive"])
+            colloid_potentials.add_particle(radius=parameters["radius_positive"],
+                                            surface_potential=parameters["surface_potential_positive"])
         else:
-            assert type == "N"
-            system.addParticle(mass_negative)
-            colloid_potentials.add_particle(radius=radius_negative, surface_potential=surface_potential_negative)
+            assert t == "N"
+            system.addParticle(parameters["mass_negative"])
+            colloid_potentials.add_particle(radius=parameters["radius_negative"],
+                                            surface_potential=parameters["surface_potential_negative"])
     for force in colloid_potentials.yield_potentials():
         system.addForce(force)
 
     simulation = app.Simulation(topology, system, integrator, platform)
     simulation.context.setPositions(positions)
-    simulation.context.setVelocitiesToTemperature(parameters.temperature, 1)
+    simulation.context.setVelocitiesToTemperature(parameters["colloid_potentials_parameters"].temperature, 1)
 
     start_time = time.perf_counter_ns()
     simulation.step(number_steps)
@@ -105,12 +105,41 @@ def main(platform_name: str = "Reference", potentials: str = "algebraic", use_lo
           f"potentials: {potentials}, use_log: {use_log}, number_steps: {number_steps})")
 
 
+# See https://stackoverflow.com/questions/60979532/argparse-ignore-positional-arguments-if-a-flag-is-set
+# Run each benchmark in a new subprocess so that openmm is properly reset between runs.
+class BenchmarkAction(argparse.Action):
+    def __init__(self, option_strings, dest, **kwargs):
+        super().__init__(option_strings, dest, nargs=0, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        for use_log in (False, True):
+            for platform, number_steps in zip(("Reference", "CPU", "OpenCL", "CUDA"), (10, 1000, 1000, 1000)):
+                for potentials in ("algebraic", "tabulated"):
+                    try:
+                        subprocess.run(f"python {__file__} {platform} {potentials} {use_log} {number_steps}",
+                                       shell=True, check=True, stderr=subprocess.PIPE, text=True)
+                    except subprocess.CalledProcessError as err:
+                        print(err.stderr.strip())
+            print()
+        parser.exit()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Benchmark OpenMM for colloids system.")
+    parser.add_argument("--all", action=BenchmarkAction,
+                        help="run benchmark with various parameters")
+    parser.add_argument("platform", help="OpenMM platform to use", type=str,
+                        choices=("Reference", "CPU", "OpenCL", "CUDA"))
+    parser.add_argument("potentials", help="colloid potentials to use", type=str,
+                        choices=("algebraic", "tabulated"))
+    parser.add_argument("use_log", help="use logarithmic potentials", type=bool,
+                        choices=(False, True))
+    parser.add_argument("number_steps", help="number of time steps to run", type=int)
+    args = parser.parse_args()
+    assert args.number_steps > 0
+    benchmark_openmm(platform_name=args.platform, potentials=args.potentials, use_log=args.use_log,
+                     number_steps=args.number_steps)
+
+
 if __name__ == '__main__':
-    for use_log in (False, True):
-        for platform, number_steps in zip(("Reference", "CPU", "OpenCL", "CUDA"), (10, 1000, 1000, 1000)):
-            for potentials in ("algebraic", "tabulated"):
-                try:
-                    main(platform_name=platform, potentials=potentials, use_log=use_log, number_steps=number_steps)
-                except openmm.OpenMMException as err:
-                    print(err)
-        print()
+    main()

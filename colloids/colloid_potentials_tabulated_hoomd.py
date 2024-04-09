60b3ca7bc44864b1420334407c3f2c8d8aa97a4b
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2024-04-09 19:32:53 -0400

    Minor changes in Hoomd potential

--- colloids/colloid_potentials_tabulated_hoomd.py
+++ colloids/colloid_potentials_tabulated_hoomd.py
@@ -8,6 +8,10 @@ from colloids.colloid_potentials_parameters import ColloidPotentialsParameters
 
 
 class ColloidPotentialsTabulatedHoomd(object):
+
+    _nanometer = unit.nano * unit.meter
+    _millivolt = unit.milli * unit.volt
+
     def __init__(self, radius_one: float, radius_two: float, surface_potential_one: float, surface_potential_two: float,
                  type_one: str, type_two: str, colloid_potentials_parameters: ColloidPotentialsParameters,
                  neighbor_list: hoomd.md.nlist.nlist, shift: bool = True):
@@ -18,7 +22,7 @@ class ColloidPotentialsTabulatedHoomd(object):
         self._surface_potential_two = surface_potential_two
         self._type_one = type_one
         self._type_two = type_two
-        self._maximum_surface_separation = 20.0 * self._parameters.debye_length.value_in_unit(unit.nanometer)
+        self._maximum_surface_separation = 20.0 * self._parameters.debye_length.value_in_unit(self._nanometer)
         self._number_samples = 5000
         self._neighbor_list = neighbor_list
         self._shift = shift
@@ -31,7 +35,7 @@ class ColloidPotentialsTabulatedHoomd(object):
         Return the steric potential from the Alexander-de Gennes polymer brush model for the given surface-to-surface
         separations.
         """
-        double_brush_length = 2.0 * self._parameters.brush_length.value_in_unit(unit.nanometer)
+        double_brush_length = 2.0 * self._parameters.brush_length.value_in_unit(self._nanometer)
         h_over_double_brush_length = h_values / double_brush_length
         double_brush_length_over_h = double_brush_length / h_values
         return (prefactor * np.where(h_values <= double_brush_length,
@@ -48,7 +52,7 @@ class ColloidPotentialsTabulatedHoomd(object):
             self, prefactor: float, h_values: npt.NDArray[np.floating], h_cut: float) -> (npt.NDArray[np.floating],
                                                                                           npt.NDArray[np.floating]):
         """Return the electrostatic potential from DLVO theory for the given surface-to-surface separations."""
-        debye_length = self._parameters.debye_length.value_in_unit(unit.nanometer)
+        debye_length = self._parameters.debye_length.value_in_unit(self._nanometer)
         if self._shift:
             a = prefactor / debye_length * np.exp(-h_cut / debye_length)
             b = -prefactor * np.exp(-h_cut / debye_length) - h_cut * a
@@ -68,35 +72,35 @@ class ColloidPotentialsTabulatedHoomd(object):
     def _set_up_tabulated_potential(self):
         steric_prefactor_11 = (
                 unit.BOLTZMANN_CONSTANT_kB * self._parameters.temperature * 16.0 * math.pi *
-                self._radius_one * unit.nanometer *
+                self._radius_one * self._nanometer *
                 (self._parameters.brush_length ** 2) * (self._parameters.brush_density ** (3 / 2)) / 35.0
                 * unit.AVOGADRO_CONSTANT_NA).value_in_unit(unit.kilojoule_per_mole)
         steric_prefactor_22 = (
                 unit.BOLTZMANN_CONSTANT_kB * self._parameters.temperature * 16.0 * math.pi *
-                self._radius_two * unit.nanometer *
+                self._radius_two * self._nanometer *
                 (self._parameters.brush_length ** 2) * (self._parameters.brush_density ** (3 / 2)) / 35.0
                 * unit.AVOGADRO_CONSTANT_NA).value_in_unit(unit.kilojoule_per_mole)
         steric_prefactor_12 = (
                 unit.BOLTZMANN_CONSTANT_kB * self._parameters.temperature * 16.0 * math.pi *
-                (self._radius_one + self._radius_two) / 2.0 * unit.nanometer *
+                (self._radius_one + self._radius_two) / 2.0 * self._nanometer *
                 (self._parameters.brush_length ** 2) * (self._parameters.brush_density ** (3 / 2)) / 35.0
                 * unit.AVOGADRO_CONSTANT_NA).value_in_unit(unit.kilojoule_per_mole)
 
         electrostatic_prefactor_11 = (
                 2.0 * math.pi * self._parameters.VACUUM_PERMITTIVITY * self._parameters.dielectric_constant
                 * self._radius_one * self._surface_potential_one * self._surface_potential_one
-                * unit.nanometer * (unit.milli * unit.volt) ** 2
+                * self._nanometer * self._millivolt ** 2
                 * unit.AVOGADRO_CONSTANT_NA).value_in_unit(unit.kilojoule_per_mole)
         electrostatic_prefactor_22 = (
                 2.0 * math.pi * self._parameters.VACUUM_PERMITTIVITY * self._parameters.dielectric_constant
                 * self._radius_two * self._surface_potential_two * self._surface_potential_two
-                * unit.nanometer * (unit.milli * unit.volt) ** 2
+                * self._nanometer * self._millivolt ** 2
                 * unit.AVOGADRO_CONSTANT_NA).value_in_unit(unit.kilojoule_per_mole)
         electrostatic_prefactor_12 = (
                 2.0 * math.pi * self._parameters.VACUUM_PERMITTIVITY * self._parameters.dielectric_constant
                 * 2.0 / (1.0 / self._radius_one + 1.0 / self._radius_two)
                 * self._surface_potential_one * self._surface_potential_two
-                * unit.nanometer * (unit.milli * unit.volt) ** 2
+                * self._nanometer * self._millivolt ** 2
                 * unit.AVOGADRO_CONSTANT_NA).value_in_unit(unit.kilojoule_per_mole)
 
         potential = hoomd.md.pair.table(width=self._number_samples, nlist=self._neighbor_list)

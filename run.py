1a70e422a25d820ae9943f3efa52b5088b37b69f
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2024-04-09 20:50:01 -0400

    First version of runner script

---
+++ run.py
@@ -0,0 +1,114 @@
+import argparse
+import numpy as np
+import openmm
+from openmm import app
+from openmm import unit
+from colloids import ColloidPotentialsAlgebraic, ColloidPotentialsParameters
+from colloids.helper_functions import read_xyz_file, write_gsd_file, write_xyz_file
+from colloids.run_parameters import RunParameters
+
+
+class ExampleAction(argparse.Action):
+    def __init__(self, option_strings, dest, **kwargs):
+        super().__init__(option_strings, dest, nargs=0, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        # TODO ADD OPTION FOR PLATFORM PROPERTIES?
+        # TODO PUT EQUILIBRATION STEPS?
+        default_parameters = RunParameters()
+        default_parameters.to_yaml("example.yaml")
+        parser.exit()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Run OpenMM for a colloids system.")
+    parser.add_argument("yaml_file", help="YAML file with simulation parameters", type=str)
+    parser.add_argument("--example", help="write an example YAML file and exit", action=ExampleAction)
+    args = parser.parse_args()
+
+    parameters = RunParameters.from_yaml(args.yaml_file)
+
+    types, positions = read_xyz_file(parameters.initial_configuration)
+
+    topology = app.topology.Topology()
+    chain = topology.addChain()
+    residue = topology.addResidue("res1", chain)
+
+    for t, position in zip(types, positions):
+        topology.addAtom(t, None, residue)
+
+    topology.setPeriodicBoxVectors(np.array(
+        [[parameters.side_length.value_in_unit(unit.nano * unit.meter), 0.0, 0.0],
+         [0.0, parameters.side_length.value_in_unit(unit.nano * unit.meter), 0.0],
+         [0.0, 0.0, parameters.side_length.value_in_unit(unit.nano * unit.meter)]]))
+
+    system = openmm.System()
+    system.setDefaultPeriodicBoxVectors(
+        openmm.Vec3(parameters.side_length.value_in_unit(unit.nano * unit.meter), 0.0, 0.0),
+        openmm.Vec3(0.0, parameters.side_length.value_in_unit(unit.nano * unit.meter), 0.0),
+        openmm.Vec3(0.0, 0.0, parameters.side_length.value_in_unit(unit.nano * unit.meter)))
+    # Prevent printing the traceback when the platform is not existing.
+    platform = openmm.Platform.getPlatformByName(parameters.platform_name)
+
+    # TODO: ALLOW FOR DIFFERENT INTEGRATORS?
+    integrator = openmm.LangevinMiddleIntegrator(parameters.temperature,
+                                                 parameters.collision_rate,
+                                                 parameters.timestep)
+    if parameters.integrator_seed is not None:
+        integrator.setRandomNumberSeed(parameters.integrator_seed)
+
+    potentials_parameters = ColloidPotentialsParameters(
+        brush_density=parameters.brush_density, brush_length=parameters.brush_length,
+        debye_length=parameters.debye_length, temperature=parameters.temperature,
+        dielectric_constant=parameters.dielectric_constant
+    )
+    colloid_potentials = ColloidPotentialsAlgebraic(
+        colloid_potentials_parameters=potentials_parameters, use_log=parameters.use_log)
+
+    for t, position in zip(types, positions):
+        system.addParticle(parameters.masses[t])
+        colloid_potentials.add_particle(radius=parameters.radii[t],
+                                        surface_potential=parameters.surface_potentials[t])
+    for force in colloid_potentials.yield_potentials():
+        system.addForce(force)
+
+    if parameters.platform_name == "CUDA" or parameters.platform_name == "OpenCL":
+        simulation = app.Simulation(topology, system, integrator, platform,
+                                    platformProperties={"Precision": "mixed"})
+    else:
+        simulation = app.Simulation(topology, system, integrator, platform)
+    simulation.context.setPositions(positions)
+
+    if parameters.velocity_seed is not None:
+        simulation.context.setVelocitiesToTemperature(parameters.temperature,
+                                                      parameters.velocity_seed)
+    else:
+        simulation.context.setVelocitiesToTemperature(parameters.temperature)
+
+    if parameters.minimize_energy_initially:
+        # TODO: Do we want this?
+        # Add reporter during minimization?
+        # See https://openmm.github.io/openmm-cookbook/dev/notebooks/cookbook/report_minimization.html
+        simulation.minimizeEnergy()
+
+    simulation.reporters.append(app.DCDReporter(parameters.trajectory_filename, parameters.trajectory_interval))
+    simulation.reporters.append(app.StateDataReporter(parameters.state_data_filename,
+                                                      parameters.state_data_interval, time=True,
+                                                      kineticEnergy=True, potentialEnergy=True, temperature=True,
+                                                      speed=True))
+    simulation.reporters.append(app.CheckpointReporter(parameters.checkpoint_filename,
+                                                       parameters.checkpoint_interval))
+
+    simulation.step(parameters.run_steps)
+    # TODO: Automatically plot energies etc.
+    # TODO: CHECK ALL SURFACE SEPARATIONS
+
+    if parameters.final_configuration_gsd_filename is not None:
+        write_gsd_file(parameters.final_configuration_gsd_filename, simulation, parameters.radii)
+
+    if parameters.final_configuration_xyz_filename is not None:
+        write_xyz_file(parameters.final_configuration_xyz_filename, simulation)
+
+
+if __name__ == '__main__':
+    main()

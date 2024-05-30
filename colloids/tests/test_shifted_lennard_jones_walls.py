b91c1660abbbd836cabfe7b40c1ae9fa961839a6
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2024-05-30 12:21:54 -0400

    Removed box_length from unittest

--- colloids/tests/test_shifted_lennard_jones_walls.py
+++ colloids/tests/test_shifted_lennard_jones_walls.py
@@ -6,11 +6,11 @@ import numpy as np
 
 class TestShiftedLennardJonesWallsParameters(object):
     @staticmethod
-    def slj_walls_potential_active(pos, box_length, rcut, delta, epsilon, sigma, alpha):
-        return np.where(np.abs(pos) < box_length / 2 - rcut - delta,
+    def slj_walls_potential_active(pos, wall_distance, rcut, delta, epsilon, sigma, alpha):
+        return np.where(np.abs(pos) < wall_distance / 2 - rcut - delta,
                         0.0,
-                        4 * epsilon * (np.power(sigma / (box_length / 2 - np.abs(pos) - delta), 12)
-                                       - alpha * np.power(sigma / (box_length / 2 - np.abs(pos) - delta), 6))
+                        4 * epsilon * (np.power(sigma / (wall_distance / 2 - np.abs(pos) - delta), 12)
+                                       - alpha * np.power(sigma / (wall_distance / 2 - np.abs(pos) - delta), 6))
                         - 4 * epsilon * (np.power(sigma / rcut, 12)
                                          - alpha * np.power(sigma / rcut, 6)))
 
@@ -19,7 +19,7 @@ class TestShiftedLennardJonesWallsParameters(object):
         return 105.0 * (unit.nano * unit.meter)
 
     @pytest.fixture
-    def box_lengths(self):
+    def wall_distances(self):
         return [1000.0 * (unit.nano * unit.meter), 1500.0 * (unit.nano * unit.meter), 1200.0 * (unit.nano * unit.meter)]
 
     @pytest.fixture
@@ -43,11 +43,11 @@ class TestShiftedLennardJonesWallsParameters(object):
         return 1000
 
     @pytest.fixture
-    def test_positions(self, box_lengths, num_test_values):
+    def test_positions(self, wall_distances, num_test_values):
         # noinspection PyUnresolvedReferences
-        return [np.linspace(-box_length.value_in_unit(unit.nanometer) / 2.0 + 200.0,
-                            box_length.value_in_unit(unit.nanometer) / 2.0 - 200.0, num=num_test_values)
-                for box_length in box_lengths]
+        return [np.linspace(-wall_distance.value_in_unit(unit.nanometer) / 2.0 + 200.0,
+                            wall_distance.value_in_unit(unit.nanometer) / 2.0 - 200.0, num=num_test_values)
+                for wall_distance in wall_distances]
 
     @pytest.fixture
     def all_wall_directions(self):
@@ -58,11 +58,11 @@ class TestShiftedLennardJonesWallsParameters(object):
         return [False, True, False]
 
     @pytest.fixture
-    def openmm_system(self, box_lengths):
+    def openmm_system(self, wall_distances):
         system = System()
-        system.setDefaultPeriodicBoxVectors(Vec3(box_lengths[0], 0.0, 0.0),
-                                            Vec3(0.0, box_lengths[1], 0.0),
-                                            Vec3(0.0, 0.0, box_lengths[2]))
+        system.setDefaultPeriodicBoxVectors(Vec3(wall_distances[0], 0.0, 0.0),
+                                            Vec3(0.0, wall_distances[1], 0.0),
+                                            Vec3(0.0, 0.0, wall_distances[2]))
         return system
 
     @pytest.fixture
@@ -74,13 +74,13 @@ class TestShiftedLennardJonesWallsParameters(object):
         return LangevinIntegrator(0.0, 0.0, 0.0)
 
     @pytest.fixture
-    def slj_potential_all(self, box_lengths, epsilon, alpha_all, all_wall_directions):
-        return ShiftedLennardJonesWalls(box_lengths, epsilon, alpha_all, all_wall_directions)
+    def slj_potential_all(self, wall_distances, epsilon, alpha_all, all_wall_directions):
+        return ShiftedLennardJonesWalls(wall_distances, epsilon, alpha_all, all_wall_directions)
 
     @pytest.fixture
-    def slj_potential_some(self, box_lengths, epsilon, alpha_some, some_wall_directions):
-        new_box_lengths = [None if not i else box_lengths[j] for j, i in enumerate(some_wall_directions)]
-        return ShiftedLennardJonesWalls(new_box_lengths, epsilon, alpha_some, some_wall_directions)
+    def slj_potential_some(self, wall_distances, epsilon, alpha_some, some_wall_directions):
+        new_wall_distances = [None if not i else wall_distances[j] for j, i in enumerate(some_wall_directions)]
+        return ShiftedLennardJonesWalls(new_wall_distances, epsilon, alpha_some, some_wall_directions)
 
 
 class TestShiftedLennardJonesWallsExceptions(TestShiftedLennardJonesWallsParameters):
@@ -140,7 +140,7 @@ class TestShiftedLennardJonesWallPotentialsAll(TestShiftedLennardJonesWallsParam
                                  (1, TestShiftedLennardJonesWallsParameters.slj_walls_potential_active),
                                  (2, TestShiftedLennardJonesWallsParameters.slj_walls_potential_active)
                              ])
-    def test_slj_walls_potential(self, openmm_context, test_positions, box_lengths, epsilon, alpha_all, radius,
+    def test_slj_walls_potential(self, openmm_context, test_positions, wall_distances, epsilon, alpha_all, radius,
                                  all_wall_directions, direction, expected_function):
         openmm_potentials = np.empty(len(test_positions[direction]))
         for index, dir_position in enumerate(test_positions[direction]):
@@ -151,10 +151,10 @@ class TestShiftedLennardJonesWallPotentialsAll(TestShiftedLennardJonesWallsParam
             openmm_potentials[index] = openmm_state.getPotentialEnergy().value_in_unit(unit.kilojoule_per_mole)
 
         assert all_wall_directions[direction]
-        box_length = box_lengths[direction].value_in_unit(unit.nano * unit.meter)
+        wall_distance = wall_distances[direction].value_in_unit(unit.nano * unit.meter)
         rcut = radius.value_in_unit(unit.nano * unit.meter) * 2**(1.0/6.0)
         delta = radius.value_in_unit(unit.nano * unit.meter) - 1.0
-        expected_potentials = expected_function(test_positions[direction], box_length, rcut, delta,
+        expected_potentials = expected_function(test_positions[direction], wall_distance, rcut, delta,
                                                 epsilon.value_in_unit(unit.kilojoule_per_mole),
                                                 radius.value_in_unit(unit.nano * unit.meter), alpha_all)
         assert np.any(expected_potentials > 0.0)
@@ -182,7 +182,7 @@ class TestShiftedLennardJonesWallPotentialsSome(TestShiftedLennardJonesWallsPara
                                  (1, TestShiftedLennardJonesWallsParameters.slj_walls_potential_active),
                                  (2, lambda pos, *args: np.zeros_like(pos))
                              ])
-    def test_slj_walls_potential(self, openmm_context, test_positions, box_lengths, epsilon, alpha_some, radius,
+    def test_slj_walls_potential(self, openmm_context, test_positions, wall_distances, epsilon, alpha_some, radius,
                                  some_wall_directions, direction, expected_function):
         openmm_potentials = np.empty(len(test_positions[direction]))
         for index, dir_position in enumerate(test_positions[direction]):
@@ -192,10 +192,10 @@ class TestShiftedLennardJonesWallPotentialsSome(TestShiftedLennardJonesWallsPara
             openmm_state = openmm_context.getState(getEnergy=True)
             openmm_potentials[index] = openmm_state.getPotentialEnergy().value_in_unit(unit.kilojoule_per_mole)
 
-        box_length = box_lengths[direction].value_in_unit(unit.nano * unit.meter)
+        wall_distance = wall_distances[direction].value_in_unit(unit.nano * unit.meter)
         rcut = radius.value_in_unit(unit.nano * unit.meter) * 2 ** (1.0 / 6.0)
         delta = radius.value_in_unit(unit.nano * unit.meter) - 1.0
-        expected_potentials = expected_function(test_positions[direction], box_length, rcut, delta,
+        expected_potentials = expected_function(test_positions[direction], wall_distance, rcut, delta,
                                                 epsilon.value_in_unit(unit.kilojoule_per_mole),
                                                 radius.value_in_unit(unit.nano * unit.meter), alpha_some)
         if some_wall_directions[direction]:

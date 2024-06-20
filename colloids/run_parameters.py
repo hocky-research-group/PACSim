b013f677257da5e3d1c8c03614041d8c415a59fb
ns3539 <ns3539@nyu.edu>
2024-06-19 22:08:06 -0400

    test 3

--- colloids/run_parameters.py
+++ colloids/run_parameters.py
@@ -253,10 +253,10 @@ class RunParameters(Parameters):
         if self.platform_name not in ["Reference", "CPU", "CUDA", "OpenCL"]:
             raise ValueError("The platform name must be 'Reference', 'CPU', 'CUDA', or 'OpenCL'.")
         
-        integrator_options = inspect.getmembers(Integrators, predicate=inspect.ismethod)
+        integrator_options = inspect.getmembers(Integrators) #, predicate=inspect.ismethod)
         print(integrator_options)
         
-        if self.integrator not in inspect.getmembers(Integrators, predicate=inspect.ismethod):
+        if self.integrator not in inspect.getmembers(Integrators): #, predicate=inspect.ismethod):
             print(self.integrator)
             print(type(self.integrator))
             raise ValueError("The integrator must be one of the following: 'BrownianIntegrator', 'LangevinIntegrator',"

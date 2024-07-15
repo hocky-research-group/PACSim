331448fab52f1ffbcf3c8e46f8c5fe29d6dd867f
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2024-07-15 13:09:23 -0400

    Updated LJ script

--- gstem/initial_particle_positions.py
+++ gstem/initial_particle_positions.py
@@ -1,6 +1,7 @@
 """
 Code taken from https://openmmtools.readthedocs.io/en/stable/_modules/openmmtools/testsystems.html#LennardJonesFluid
 """
+import itertools
 import numpy as np
 from openmm import unit
 
@@ -32,14 +33,14 @@ def halton_sequence(p, n):
     u = np.empty(n)
     for j in range(n):
         i = 0
-        b[0] += 1                       # add one to current integer
-        while b[i] > p - 1 + eps:           # this loop does carrying in base p
+        b[0] += 1  # add one to current integer
+        while b[i] > p - 1 + eps:  # this loop does carrying in base p
             b[i] = 0
             i = i + 1
             b[i] += 1
         u[j] = 0
-        for k in range(len(b)):         # add up reversed digits
-            u[j] += b[k] * p**-(k + 1)
+        for k in range(len(b)):  # add up reversed digits
+            u[j] += b[k] * p ** -(k + 1)
     return u
 
 
@@ -68,3 +69,53 @@ def subrandom_particle_positions(nparticles, box_vectors):
         positions[:, dim] = unit.Quantity(x * length / length.unit, length.unit)
 
     return positions
+
+
+def build_lattice_cell():
+    """Build a single (4 atom) unit cell of a FCC lattice, assuming a cell length
+    of 1.0.
+
+    Returns
+    -------
+    xyz : np.ndarray, shape=(4, 3), dtype=float
+        Coordinates of each particle in cell
+    """
+    xyz = [[0, 0, 0], [0, 0.5, 0.5], [0.5, 0.5, 0], [0.5, 0, 0.5]]
+    xyz = np.array(xyz)
+
+    return xyz
+
+
+def build_lattice(n_particles):
+    n = ((n_particles / 4.) ** (1 / 3.))
+
+    if np.abs(n - np.round(n)) > 1E-10:
+        raise ValueError("Must input 4 m^3 particles for some integer m!")
+    else:
+        n = int(np.round(n))
+
+    xyz = []
+    cell = build_lattice_cell()
+    x, y, z = np.eye(3)
+    for atom, (i, j, k) in enumerate(itertools.product(np.arange(n), repeat=3)):
+        xi = cell + i * x + j * y + k * z
+        xyz.append(xi)
+
+    xyz = np.concatenate(xyz)
+
+    return xyz, n
+
+
+def lattice_particle_positions(nparticles, box_vectors):
+    assert len(box_vectors) == 3
+    assert len(box_vectors[0]) == 3
+    assert len(box_vectors[1]) == 3
+    assert len(box_vectors[2]) == 3
+    assert box_vectors[0][0] == box_vectors[1][1] == box_vectors[2][2]
+    assert box_vectors[0][1] == box_vectors[0][2] == 0.0 * unit.nanometer
+    assert box_vectors[1][0] == box_vectors[1][2] == 0.0 * unit.nanometer
+    assert box_vectors[2][0] == box_vectors[2][1] == 0.0 * unit.nanometer
+    box_nm = box_vectors[0][0] / unit.nanometer
+    xyz, box = build_lattice(nparticles)
+    xyz *= (box_nm / box)
+    return xyz
--- gstem/lennard_jones.py
+++ gstem/lennard_jones.py
@@ -3,26 +3,27 @@ from openmm import app
 from openmm import unit
 from colloids.gsd_reporter import GSDReporter
 from colloids.status_reporter import StatusReporter
-from initial_particle_positions import subrandom_particle_positions
+from initial_particle_positions import subrandom_particle_positions, lattice_particle_positions
 
 
 def main():
-    number_particles = 10000
-    reduced_density = 0.05
+    number_particles = 10976
+    reduced_density = 0.5
     mass = 39.9 * unit.amu  # argon
     sigma = 3.4 * unit.angstrom  # argon
     epsilon = 0.238 * unit.kilocalories_per_mole  # argon
-    cutoff = 3.0 * sigma
+    cutoff = 7.0 * sigma
     switch_width = 3.4 * unit.angstrom  # argon
     temperature = epsilon / unit.BOLTZMANN_CONSTANT_kB / unit.AVOGADRO_CONSTANT_NA  # kT/epsilon = 1
     reduced_time_step = 0.005
     number_equilibration_steps = 100000
     number_production_steps = 1000000
-    platform = "OpenCL"  # "Reference", "CPU", "CUDA", or "OpenCL"
+    platform = "CPU"  # "Reference", "CPU", "CUDA", or "OpenCL"
     trajectory_filename = "trajectory.gsd"
     trajectory_interval = 1000
     state_data_filename = "state_data.csv"
     state_data_interval = 100
+    initial = "lattice"  # "lattice" or "random"
 
     # Create topology with Argon atoms.
     topology = app.Topology()
@@ -44,18 +45,22 @@ def main():
     )
 
     # Create Lennard-Jones force with periodic boundary conditions.
-    lennard_jones_force = openmm.NonbondedForce()
+    lennard_jones_force = openmm.CustomNonbondedForce(
+        "4.0 * epsilon / (alpha * alpha) "
+        "* (1.0 / (r * r / (sigma * sigma) - 1.0 )^6 - alpha / (r * r / (sigma * sigma) - 1.0)^3 )")
+    lennard_jones_force.addGlobalParameter("sigma", sigma)
+    lennard_jones_force.addGlobalParameter("epsilon", epsilon)
+    lennard_jones_force.addGlobalParameter("alpha", 50)
     lennard_jones_force.setNonbondedMethod(openmm.NonbondedForce.CutoffPeriodic)
     lennard_jones_force.setCutoffDistance(cutoff)
     lennard_jones_force.setUseSwitchingFunction(True)
-    lennard_jones_force.setUseDispersionCorrection(True)
     lennard_jones_force.setSwitchingDistance(cutoff - switch_width)
 
     # Add particles to the system and the Lennard-Jones force.
     for particle_index in range(number_particles):
         system.addParticle(mass)
         # Set charge to zero to switch off electrostatics.
-        lennard_jones_force.addParticle(0.0, sigma, epsilon)
+        lennard_jones_force.addParticle()
 
     # Add the Lennard-Jones force to the system.
     system.addForce(lennard_jones_force)
@@ -69,7 +74,11 @@ def main():
     simulation = app.Simulation(topology, system, integrator, platform_object)
 
     # Set initial positions and velocities, and minimize energy of initial configuration.
-    positions = subrandom_particle_positions(number_particles, system.getDefaultPeriodicBoxVectors())
+    if initial == "random":
+        positions = subrandom_particle_positions(number_particles, system.getDefaultPeriodicBoxVectors())
+    else:
+        positions = lattice_particle_positions(number_particles, system.getDefaultPeriodicBoxVectors())
+
     simulation.context.setPositions(positions)
     simulation.minimizeEnergy()
     simulation.context.setVelocitiesToTemperature(temperature)

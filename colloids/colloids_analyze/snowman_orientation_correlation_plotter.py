f446a771154f29ed0d54d4f358196f3fa3c9e63d
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2024-12-27 10:28:08 +0100

    Added some temporary fitting

--- colloids/colloids_analyze/snowman_orientation_correlation_plotter.py
+++ colloids/colloids_analyze/snowman_orientation_correlation_plotter.py
@@ -5,6 +5,7 @@ import MDAnalysis
 import MDAnalysis.analysis
 import numpy as np
 from openmm import unit
+from scipy.optimize import curve_fit
 from colloids.colloids_analyze import LabeledRunParametersWithPath, Plotter
 
 
@@ -34,13 +35,21 @@ class SnowmanOrientationCorrelationPlotter(Plotter):
             corr = np.fft.ifft(sf).real
             corr = corr[:n] / np.arange(n, 0, -1)
             autocorr += corr
-
+        print(autocorr[0])
         return autocorr / autocorr[0]
+
+    @staticmethod
+    def decay(t, a, b):
+        return np.exp(-a * (t - b + b * np.exp(-t / b)))
 
     def plot(self) -> None:
         with PdfPages(self._working_directory / "orientation_autocorrelation.pdf") as pdf:
             full_mean_autocorrelation_functions = {}
+            max_index = None
+            fits = {}
+            x_datas = {}
             for index, rp in enumerate(self._run_parameters):
+                print(f"Processing: {rp.run_parameters.trajectory_filename}")
                 # If run_parameters["parameters"].trajectory_filename is a complete path, the division operator of paths
                 # only returns that complete path. Otherwise, this combines base_path and path.
                 trajectory_path = rp.path / rp.run_parameters.trajectory_filename
@@ -81,31 +90,50 @@ class SnowmanOrientationCorrelationPlotter(Plotter):
                             coords[body_index, frame_index] /= np.linalg.norm(coords[body_index, frame_index])
                             assert np.linalg.norm(coords[body_index, frame_index]) - 1.0 < 1.0e-12
 
-                    autocorrelation_functions = np.empty(coords.shape[:2], dtype=float)
+                    autocorrelation_functions = np.zeros(coords.shape[:2], dtype=float)
                     fig = plt.figure()
                     for body_index in range(len(snowman_body_group)):
                         autocorrelation_functions[body_index] = self.autocorr_fft(coords[body_index])
                         plt.plot(autocorrelation_functions[body_index], color="C0", alpha=0.1)
                     full_mean_autocorrelation_functions[snowman_body_type + ", " + rp.label] = np.mean(
                         autocorrelation_functions, axis=0)
+
+                    # TODO: REMOVE LATEST COMMIT AGAIN
+                    x_data = np.arange(300)  # Free: 36, R10: 80, R30: 300, R40: 600, R60: 600
+                    fit = curve_fit(self.decay, x_data,
+                                    full_mean_autocorrelation_functions[snowman_body_type + ", " + rp.label][:len(x_data)])
+                    fits[snowman_body_type + ", " + rp.label] = fit
+                    x_datas[snowman_body_type + ", " + rp.label] = x_data
+
                     plt.plot(full_mean_autocorrelation_functions[snowman_body_type + ", " + rp.label], color="C1",
                              marker=".")
-                    plt.title(f"{rp.label}, Snowman body type: {snowman_body_type}")
+                    plt.plot(x_data, self.decay(x_data, *fit[0]), color="C2", label=f"Fit: {fit[0]}")
+                    plt.title(f"{rp.label}, Snowman body type: {snowman_body_type}, "
+                              f"Trajectory length: {len(universe.trajectory) - self._start_frame}")
                     plt.xlabel(r"$\Delta t$")
                     plt.ylabel(r"$\langle \hat{\mathbf{r}}_i(t)\cdot\hat{\mathbf{r}}_i(t + \Delta t) \rangle$")
                     plt.yscale("log")
-                    #plt.xlim(0, np.where(full_mean_autocorrelation_functions[snowman_body_type + ", " + rp.label] < 1.0e-2)[0][0])
-                    #plt.ylim(1.0e-2, 1.0)
+                    if max_index is None:
+                        max_index = (len(universe.trajectory) - self._start_frame) // 10
+                    else:
+                        max_index = max(max_index, (len(universe.trajectory) - self._start_frame) // 10)
+                    plt.xlim(0, (len(universe.trajectory) - self._start_frame) // 10)
+                    plt.ylim(1.0e-3, 1.0)
                     pdf.savefig(fig)
                     fig.clear()
                     plt.close(fig)
 
             fig = plt.figure()
-            for label, autocorr in full_mean_autocorrelation_functions.items():
-                plt.plot(autocorr, label=label, marker=".")
+
+            for i, (label, autocorr) in enumerate(full_mean_autocorrelation_functions.items()):
+                plt.plot(autocorr, label=label, marker=".", linestyle="dashed", color=f"C{i}")
+                plt.plot(x_datas[label], self.decay(x_datas[label], *fits[label][0]),
+                         label=f"{label} fit: {fits[label][0]}", color=f"C{i}")
             plt.xlabel(r"$\Delta t$")
             plt.ylabel(r"$\langle \hat{\mathbf{r}}_i(t)\cdot\hat{\mathbf{r}}_i(t + \Delta t) \rangle$")
             plt.yscale("log")
+            plt.xlim(0, max_index)
+            plt.ylim(1.0e-3, 1.0)
             plt.legend()
             pdf.savefig(fig)
             fig.clear()

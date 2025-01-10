2ff91b5b37a7997484c462985134b308850d9168
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2025-01-10 12:00:53 +0100

    Updated run parameters test

--- colloids/tests/test_run_parameters.py
+++ colloids/tests/test_run_parameters.py
@@ -30,7 +30,7 @@ class TestQuantity(object):
 class TestRunParameters(object):
     @pytest.fixture
     def parameters(self):
-        return RunParameters(initial_configuration="first_frame.xyz")
+        return RunParameters(initial_configuration="first_frame.gsd")
 
     @pytest.fixture(autouse=True)
     def change_test_dir(self, request, monkeypatch):
@@ -51,33 +51,16 @@ class TestRunParameters(object):
     def test_run_parameters(self, parameters, yaml_parameters):
         # Because we cannot compare openmm quantities directly (see above), we have to compare all fields explicitly.
         # When new fields are added to the RunParameters dataclass, this test must be updated accordingly.
-        assert len(fields(parameters)) == len(fields(yaml_parameters)) == 44
+        assert len(fields(parameters)) == len(fields(yaml_parameters)) == 34
         assert parameters.initial_configuration == yaml_parameters.initial_configuration
-        assert len(parameters.masses) == len(yaml_parameters.masses)
-        assert len(parameters.radii) == len(yaml_parameters.radii)
-        assert len(parameters.surface_potentials) == len(yaml_parameters.surface_potentials)
-        for t in parameters.masses:
-            assert t in yaml_parameters.masses
-            assert (parameters.masses[t].value_in_unit(parameters.masses[t].unit)
-                    == pytest.approx(yaml_parameters.masses[t].value_in_unit(parameters.masses[t].unit), rel=1e-12,
-                                     abs=1e-12))
-        for t in parameters.radii:
-            assert t in yaml_parameters.radii
-            assert (parameters.radii[t].value_in_unit(parameters.radii[t].unit)
-                    == pytest.approx(yaml_parameters.radii[t].value_in_unit(parameters.radii[t].unit), rel=1e-12,
-                                     abs=1e-12))
-        for t in parameters.surface_potentials:
-            assert t in yaml_parameters.surface_potentials
-            assert (parameters.surface_potentials[t].value_in_unit(parameters.surface_potentials[t].unit)
-                    == pytest.approx(
-                        yaml_parameters.surface_potentials[t].value_in_unit(parameters.surface_potentials[t].unit),
-                        rel=1e-12, abs=1e-12))
-
+        assert parameters.frame_index == yaml_parameters.frame_index
         assert parameters.platform_name == yaml_parameters.platform_name
         assert (parameters.potential_temperature.value_in_unit(parameters.potential_temperature.unit)
                 == pytest.approx(
                     yaml_parameters.potential_temperature.value_in_unit(parameters.potential_temperature.unit),
                     rel=1e-12, abs=1e-12))
+        assert parameters.integrator == yaml_parameters.integrator
+        assert parameters.integrator_parameters == yaml_parameters.integrator_parameters
         assert (parameters.brush_density.value_in_unit(parameters.brush_density.unit)
                 == pytest.approx(yaml_parameters.brush_density.value_in_unit(parameters.brush_density.unit), rel=1e-12,
                                  abs=1e-12))
@@ -88,10 +71,8 @@ class TestRunParameters(object):
                 == pytest.approx(yaml_parameters.debye_length.value_in_unit(parameters.debye_length.unit), rel=1e-12,
                                  abs=1e-12))
         assert parameters.dielectric_constant == yaml_parameters.dielectric_constant
+        assert parameters.cutoff_factor == yaml_parameters.cutoff_factor
         assert parameters.use_log == yaml_parameters.use_log
-        assert parameters.use_tabulated == yaml_parameters.use_tabulated
-        assert parameters.integrator == yaml_parameters.integrator
-        assert parameters.integrator_parameters == yaml_parameters.integrator_parameters
         assert parameters.velocity_seed == yaml_parameters.velocity_seed
         assert parameters.run_steps == yaml_parameters.run_steps
         assert parameters.state_data_interval == yaml_parameters.state_data_interval
@@ -102,11 +83,9 @@ class TestRunParameters(object):
         assert parameters.checkpoint_filename == yaml_parameters.checkpoint_filename
         assert parameters.minimize_energy_initially == yaml_parameters.minimize_energy_initially
         assert parameters.final_configuration_gsd_filename == yaml_parameters.final_configuration_gsd_filename
-        assert parameters.final_configuration_xyz_filename == yaml_parameters.final_configuration_xyz_filename
-        assert parameters.cutoff_factor == yaml_parameters.cutoff_factor
-        assert all(pw == yw for pw, yw in zip(parameters.wall_directions, yaml_parameters.wall_directions))
-        assert parameters.alpha == yaml_parameters.alpha
         assert parameters.epsilon == yaml_parameters.epsilon
+        assert parameters.alpha == yaml_parameters.alpha
+        assert all(pw == yw for pw, yw in zip(parameters.wall_directions, yaml_parameters.wall_directions))
         assert parameters.use_depletion == yaml_parameters.use_depletion
         assert parameters.depletion_phi == yaml_parameters.depletion_phi
         assert parameters.depletant_radius == yaml_parameters.depletant_radius
@@ -116,12 +95,6 @@ class TestRunParameters(object):
         assert parameters.particle_density == yaml_parameters.particle_density
         assert parameters.update_reporter == yaml_parameters.update_reporter
         assert parameters.update_reporter_parameters == yaml_parameters.update_reporter_parameters
-        assert parameters.use_substrate == yaml_parameters.use_substrate
-        assert parameters.substrate_type == yaml_parameters.substrate_type
-        assert parameters.use_snowman == yaml_parameters.use_snowman
-        assert parameters.snowman_seed == yaml_parameters.snowman_seed
-        assert parameters.snowman_bond_types == yaml_parameters.snowman_bond_types
-        assert parameters.snowman_distances == yaml_parameters.snowman_distances
 
 
 if __name__ == '__main__':

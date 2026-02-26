fd3e985045739eff66d776d7804d7faa08f67793
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2026-02-26 17:42:34 -0500

    Refactored lattice builder

--- colloids/colloids_create/configuration_generators/lattice_builder.py
+++ colloids/colloids_create/configuration_generators/lattice_builder.py
@@ -2,6 +2,7 @@ from typing import Sequence, Union
 from gsd.hoomd import Frame
 import numpy as np
 from openmm import unit
+from pymatgen.core import Element
 from pymatgen.io.cif import CifParser
 from scipy.spatial import distance_matrix
 from .abstracts import ConfigurationGenerator
@@ -19,95 +20,100 @@ class LatticeBuilder(ConfigurationGenerator):
     A smaller test supercell is used to find the optimal scale factor before applying it to the full supercell defined
     by the lattice vector scaling matrix.
 
+    :param masses:
+        The masses dictionary with the particle types as keys and the masses as values.
+    :type masses: dict[str, unit.Quantity]
+    :param radii:
+        The radii dictionary with the particle types as keys and the radii as values.
+    :type radii: dict[str, unit.Quantity]
+    :param surface_potentials:
+        The surface potentials dictionary with the particle types as keys and the surface potentials as values.
+    :type surface_potentials: dict[str, unit.Quantity]
     :param lattice_specification:
         The .cif file that specifies the desired lattice structure.
     :type lattice_specification: str
-    :param lattice_vector_scaling_matrix:
-        A scaling matrix for transforming the lattice vectors into a supercell.
-        If only a single integer is given, the same scale factor is used in all directions.
-        Every scale factor in the matrix should be positive.
-    :type lattice_vector_scaling_matrix: Union[int, Sequence[int]]
-    :param radii:
-        The radii of the different types of colloidal particles.
-        The keys of the dictionary are the types of the colloidal particles and the values are the radii.
-        The unit of the radii must be compatible with nanometers and the values must be greater than zero.
-    :type radii: dict[str, unit.Quantity]
+    :param lattice_repeats:
+        Number of repetitions of the unit cell in each direction to create the supercell. This can be specified as a
+        single integer (if the same number of repetitions is desired in all directions) or as a sequence of three
+        integers (if different numbers of repetitions are desired in different directions).
+    :type lattice_repeats: Union[int, Sequence[int]]
     :param brush_length:
         The thickness of the brush in the Alexander-de Gennes polymer brush model [i.e., L in eq. (1)].
         The unit of the brush_length must be compatible with nanometers and the value must be greater than zero.
     :type brush_length: unit.Quantity
-    :param lattice_scale_factor:
-        Scale-up increment factor used when searching for a non-overlapping configuration.
-        The lattice scale factor must be greater than zero.
-    :type lattice_scale_factor: float
-    :param lattice_scale_start:
-        Starting scale factor for the overlap search.
-        The lattice scale start factor must be greater than zero.
-    :type lattice_scale_start: float
-    :param radii_padding_factor:
+    :param radii_padding:
         Extra gap added to the effective radii when checking for overlaps.
-        The unit of the radii padding factor should be compatible with nanometers and the value must be greater than zero.
-    :type radii_padding_factor: unit.Quantity
+        The unit of the radii padding should be compatible with nanometers and the value must be greater than or equal
+        to zero.
+    :type radii_padding: unit.Quantity
+    :param lattice_padding:
+        Extra gap added to the box dimensions.
+        The unit of the lattice padding should be compatible with nanometers and the value must be greater than or
+        equal to zero.
+    :type lattice_padding: unit.Quantity
+
+    :raises ValueError:
+        If the lattice specification file is not a .cif file.
+        If the CIF file does not contain exactly one structure.
+        If the lattice repeats is not a positive integer or a sequence of three positive integers.
+        If the brush length is not compatible with nanometers or is not greater than zero.
+        If the radii padding is not compatible with nanometers or is negative.
+        If the lattice padding is not compatible with nanometers or is negative.
     """
 
-    def __init__(self, lattice_specification: str, lattice_vector_scaling_matrix: Union[int, Sequence[int]],
-                 radii: dict[str, unit.Quantity], brush_length: unit.Quantity, lattice_scale_factor: float,
-                 lattice_scale_start: float, radii_padding_factor: unit.Quantity) -> None:
+    def __init__(self, masses: dict[str, unit.Quantity], radii: dict[str, unit.Quantity],
+                 surface_potentials: dict[str, unit.Quantity], lattice_specification: str,
+                 lattice_repeats: Union[int, Sequence[int]], brush_length: unit.Quantity,
+                 radii_padding: unit.Quantity) -> None:
         """Constructor of the LatticeBuilder class."""
-        super().__init__()
+        super().__init__(masses=masses, radii=radii, surface_potentials=surface_potentials)
         if not lattice_specification.endswith('.cif'):
             raise ValueError("The lattice specification must be a .cif file.")
         parser = CifParser(lattice_specification, site_tolerance=0.0, frac_tolerance=0.0)
-        structures = parser.parse_structures(check_occu=False)
+        structures = parser.parse_structures(check_occu=False, primitive=False)
         if len(structures) != 1:
             raise ValueError("The CIF file must contain exactly one structure.")
         self._structure = structures[0]
-        self._radii = radii  # TODO: This should just be passed as in final modifiers.
-        self._brush_length = brush_length
-        self._lattice_vector_scaling_matrix = lattice_vector_scaling_matrix
-        self._lattice_scale_factor = lattice_scale_factor
-        self._lattice_scale_start = lattice_scale_start
-        self._radii_padding_factor = radii_padding_factor
+        self._radii = radii
+        self._brush_length = brush_length  # TODO: SHOULD BE TAKEN FROM RUN.YAML
+        self._lattice_repeats = lattice_repeats
+        self._radii_padding = radii_padding
+        self._lattice_padding = lattice_padding
+        # Label atoms as their element symbols based on atomic number, e.g. 'Fe', 'O', etc.
+        self._type_map = {atomic_number: str(Element.from_Z(atomic_number))
+                          for atomic_number in np.unique(self._structure.atomic_numbers)}
+        if isinstance(self._lattice_repeats, int):
+            if not self._lattice_repeats > 0:
+                raise ValueError("The lattice repeats must be greater than zero.")
+        else:
+            if not isinstance(self._lattice_repeats, Sequence) or len(self._lattice_repeats) != 3:
+                raise ValueError("The lattice repeats must be either a single integer or a sequence of three integers.")
+            if not all(r > 0 for r in self._lattice_repeats):
+                raise ValueError("All values in the lattice repeats must be greater than zero.")
+        if not self._brush_length.unit.is_compatible(unit.nanometer):
+            raise TypeError("The brush length must have a unit that is compatible with nanometers.")
+        if not self._brush_length.value_in_unit(unit.nanometer) > 0.0:
+            raise ValueError("The brush length must have a value greater than zero.")
+        if not self._radii_padding.unit.is_compatible(unit.nanometer):
+            raise TypeError("The radii padding must have a unit that is compatible with nanometers.")
+        if not self._radii_padding.value_in_unit(unit.nanometer) >= 0.0:
+            raise ValueError("The radii padding must have a value greater than or equal to zero.")
+        if not self._lattice_padding.unit.is_compatible(unit.nanometer):
+            raise TypeError("The lattice padding must have a unit that is compatible with nanometers.")
+        if not self._lattice_padding.value_in_unit(unit.nanometer) >= 0.0:
+            raise ValueError("The lattice padding must have a value greater than or equal to zero.")
 
-    @staticmethod
-    def _set_colloid_labels(atomic_numbers):
-        """Label atoms as '1' ... 'N' based on atomic number."""
-        element_list = np.unique(atomic_numbers).tolist()
-        type_map = {atomic_number: element_list.index(atomic_number) + 1 for atomic_number in atomic_numbers}
-        type_list = [str(type_map[atomic_number]) for atomic_number in atomic_numbers]
-        return type_list
-
-    @staticmethod
-    def _check_overlap(distances, radii):
-        """Check if any particles overlap."""
-        n = len(radii)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if distances[i, j] <= (radii[i] + radii[j]):
-                    return True
-        return False
-
-    @staticmethod
-    def _give_smallest_connection(distances, radii):
+    def types(self) -> set[str]:
         """
-        Return smallest effective distance (gap between particle surfaces).
+        Return the set of particle types that will be generated by this configuration generator.
 
-        Effective distance = d - (r_i + r_j).
+        :return:
+            The set of particle types that will be generated by this configuration generator.
+        :rtype: set[str]
         """
-        n = len(radii)  # TODO: I'm sure this can be sped up.
-        min_dist = np.inf
-        min_pair = None
+        return set(self._type_map.values())
 
-        for i in range(n):
-            for j in range(i + 1, n):
-                eff = distances[i, j] - (radii[i] + radii[j])
-                if eff < min_dist:
-                    min_dist = eff
-                    min_pair = (i, j)
-
-        return min_dist, min_pair
-
-    def generate_configuration(self, test_matrix=(3, 3, 3)) -> Frame:
+    def generate_configuration(self) -> Frame:
         """
         Generate the initial positions of the colloids in a gsd.hoomd.Frame instance.
 
@@ -123,52 +129,42 @@ class LatticeBuilder(ConfigurationGenerator):
             The initial configuration of the colloids.
         :rtype: gsd.hoomd.Frame
         """
-        # --- Find optimal scale factor on a small test supercell ---
-        sc_test = self._structure.make_supercell(test_matrix, in_place=False)
-        positions_test = sc_test.cart_coords
+        # Find optimal scale factor using a small test supercell to speed up the search.
+        small_supercell = self._structure.make_supercell((3, 3, 3), in_place=False, to_unit_cell=True)
+        dists = distance_matrix(small_supercell.cart_coords, small_supercell.cart_coords)
+        effective_radii = [self._radii[self._type_map[atomic_number]].value_in_unit(unit.nanometer)
+                           + self._brush_length.value_in_unit(unit.nanometer)
+                           + self._radii_padding.value_in_unit(unit.nanometer)
+                           for atomic_number in small_supercell.atomic_numbers]
+        required_scale_factor = 0.0
+        for i in range(len(effective_radii)):
+            for j in range(i + 1, len(effective_radii)):
+                required_scale_factor = max(required_scale_factor,
+                                            (effective_radii[i] + effective_radii[j]) / dists[i, j])
+        # TODO: CAN I ASSERT THAT THIS IS CORRECT?
 
-        types_test = self._set_colloid_labels(sc_test.atomic_numbers)
+        # Apply scale factor to the full supercell.
+        structure_full = self._structure.make_supercell(self._lattice_repeats, in_place=False)
+        scaled_positions = structure_full.cart_coords * required_scale_factor
 
-        # Effective radii: colloid radius + brush length + padding
-        radii_test = [self._radii[str(t)].value_in_unit(unit.nanometer) for t in types_test]
-        radii_test = np.array(radii_test) + self._brush_length.value_in_unit(unit.nanometer) + \
-            self._radii_padding_factor.value_in_unit(unit.nanometer)
+        # Center at origin.
+        positions -= scaled_positions.mean(axis=0)
+        effective_radii = [self._radii[self._type_map[atomic_number]].value_in_unit(unit.nanometer)
+                           + self._brush_length.value_in_unit(unit.nanometer)
+                           + self._radii_padding.value_in_unit(unit.nanometer)
+                           for atomic_number in structure_full.atomic_numbers]
 
-        dists = distance_matrix(positions_test, positions_test)
-        i = 0
-        scale = self._lattice_scale_start
-
-        while self._check_overlap(dists, radii_test):  # TODO: Why not just move it?
-            scale = self._lattice_scale_start + i * self._lattice_scale_factor
-            positions_exp = positions_test * scale
-            dists = self._calculate_distances(positions_exp)
-            i += 1
-
-        # --- Apply scale factor to the full supercell ---
-        structure_full = self._structure.make_supercell(self._lattice_vector_scaling_matrix, in_place=False)
-        positions = structure_full.cart_coords * scale
-
-        # Center at origin
-        positions -= positions.mean(axis=0)
-
-        types = self._set_colloid_labels(structure_full.atomic_numbers)
-
-        # Compute effective radii for box sizing
-        radii_full = [self._radii[str(t)].value_in_unit(unit.nanometer) for t in types]
-        radii_full = np.array(radii_full) + self._brush_length.value_in_unit(unit.nanometer) + \
-            self._radii_padding_factor.value_in_unit(unit.nanometer)
-        box = np.max(positions, axis=0) + np.max(radii_full)
+        # Embed in cubic box with padding.
+        box_length = 2.0 * (np.max(np.abs(positions)) + np.max(effective_radii)
+                            + self._lattice_padding.value_in_unit(unit.nanometer))
 
         # --- Build the Frame ---
         frame = Frame()
         frame.particles.N = len(positions)
         frame.particles.types = sorted(set(types))
         frame.particles.typeid = np.array(
-            [frame.particles.types.index(t) for t in types], dtype=np.uint32
-        )
+            [frame.particles.types.index(t) for t in types], dtype=np.uint32)
         frame.particles.position = np.array(positions, dtype=np.float32)
-        frame.configuration.box = np.array(
-            [box[0], box[1], box[2], 0.0, 0.0, 0.0], dtype=np.float32
-        )
+        frame.configuration.box = np.array([box_length, box_length, box_length, 0.0, 0.0, 0.0], dtype=np.float32)
 
         return frame

fixed O(n^2) scaling

colloids/colloids_create/cluster_generator.py
@@ -164,6 +164,7 @@ class ClusterGenerator(ConfigurationGenerator):
 
         repeated_cluster = None
         bond_pairs = []
+        bond_values = []
         # All cells are assumed to be the same so we can use the first one.
         cell = centered_padded_clusters[0].get_cell()
         # Adapted from ase's repeat function.
@@ -195,10 +196,15 @@ class ClusterGenerator(ConfigurationGenerator):
                         repeated_cluster = new_cluster.copy()
                         if "bonds" in repeated_cluster.arrays:
                             # Extract the bonds and adjust the indices to account for the repetitions.
-                            bond_pairs += [(first_index, second_index)
-                                           for first_index, bonds in enumerate(new_cluster.arrays["bonds"])
-                                           for second_index in self._extract_bonded_indices(bonds)]
+                            new_bond_pairs = [(first_index, second_index)
+                                              for first_index, bonds in enumerate(new_cluster.arrays["bonds"])
+                                              for second_index in self._extract_bonded_indices(bonds)]
+                            bond_pairs += new_bond_pairs
+                            bond_values += [(new_cluster.get_distance(first_index, second_index))
+                                            for first_index, second_index in new_bond_pairs]
                     else:
+                        # Set the positions of the repeated cluster to the unwrapped positions.
+                        repeated_cluster.arrays["positions"][i0:i0 + len(new_cluster)] = unwrapped_positions
                         # Concatenate the arrays of the new cluster to the repeated cluster.
                         for name, a in new_cluster.arrays.items():
                             if name != "bonds":
@@ -209,8 +215,8 @@ class ClusterGenerator(ConfigurationGenerator):
                                 bond_pairs += [(first_index + i0, second_index + i0)
                                                for first_index, bonds in enumerate(new_cluster.arrays["bonds"])
                                                for second_index in self._extract_bonded_indices(bonds)]
-                        # Set the positions of the repeated cluster to the unwrapped positions.
-                        repeated_cluster.arrays["positions"][i0:i0 + len(new_cluster)] = unwrapped_positions
+                                bond_values += [(repeated_cluster.get_distance(first_index + i0, second_index + i0))
+                                                for first_index, second_index in self._extract_bonded_indices_with_offset(new_cluster.arrays["bonds"], i0)]
                     i0 += len(new_cluster)
 
         # Enlarge the cell of the repeated cluster.
@@ -232,10 +238,8 @@ class ClusterGenerator(ConfigurationGenerator):
              repeated_cluster.cell[2][1] / repeated_cluster.cell[2][2]], dtype=np.float32)
 
         if len(bond_pairs) > 0:
-            all_distances = repeated_cluster.get_all_distances()
             all_constraints = np.array(bond_pairs, dtype=np.uint32)
-            all_values = np.array([all_distances[first_index, second_index]
-                                   for first_index, second_index in bond_pairs], dtype=np.float32)
+            all_values = np.array(bond_values, dtype=np.float32)
             frame.constraints.N = len(bond_pairs)
             frame.constraints.group = all_constraints
             frame.constraints.value = all_values